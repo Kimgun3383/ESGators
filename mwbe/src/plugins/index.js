d147f5c18ef80493cb894eaf66e6aaefa623c9ca
Kimgun3383 <tygob5247@gmail.com>
2026-02-28 14:03:22 +0900

    Add Grafana & make dummy date for test

--- mwbe/src/plugins/index.js
+++ mwbe/src/plugins/index.js
@@ -2,6 +2,7 @@ const cors = require("@fastify/cors");
 const helmet = require("@fastify/helmet");
 const sensible = require("@fastify/sensible");
 const registerSupabase = require("./supabase");
+const registerGrafanaMetrics = require("./grafanaMetrics");
 
 function registerPlugins(app) {
   app.register(sensible);
@@ -10,6 +11,7 @@ function registerPlugins(app) {
     origin: process.env.CORS_ORIGIN || true,
   });
   registerSupabase(app);
+  registerGrafanaMetrics(app);
 }
 
 module.exports = registerPlugins;
--- mwbe/src/routes/index.js
+++ mwbe/src/routes/index.js
@@ -1,9 +1,11 @@
 const rootRoutes = require("./root");
 const usersRoutes = require("./users");
+const iotRoutes = require("./iot");
 
 function registerRoutes(app) {
   app.register(rootRoutes, { prefix: "/" });
   app.register(usersRoutes, { prefix: "/users" });
+  app.register(iotRoutes, { prefix: "/iot" });
 }
 
 module.exports = registerRoutes;
